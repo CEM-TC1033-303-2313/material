acce9b498a27b7843e0ca3b6a9003ba6dd4142d6
aram-gonzalez-tec <aram.gonzalez@tec.mx>
2023-11-15 08:40:44 -0600

    Introducción a clases en C++

--- Introducción a clases/Ejemplo_clase.cpp
+++ Introducción a clases/Ejemplo_clase.cpp
@@ -6,14 +6,42 @@ private:
     std::string raza;
     int edad;
 public:
+    //Perro() = default;
+    /*
     Perro() {
         std::cout << "Llamando al constructor..." << std::endl;
         nombre = "Firulais";
         raza = "Chihuahua";
         edad = 10;
-    }
+    }*/
+    /*
+    Perro(std::string nombre, std::string raza, int edad) {
+        this->nombre = nombre;
+        this->raza = raza;
+        this->edad = edad;
+    }*/
+    Perro(std::string nombre="Firulais", std::string raza="Chihuaha", int edad=10):
+        nombre{nombre}, raza{raza}, edad{edad} {}
+    /*
     ~Perro() {
         std::cout << "Llamando al destructor..." << std::endl;
+    }*/
+    std::string getNombre() {
+        return nombre;
+    }
+    std::string getRaza() {
+        return raza;
+    }
+    int getEdad() {
+        return edad;
+    }
+    void setNombre(std::string nuevo_nombre) {
+        if (!nuevo_nombre.empty())
+            nombre = nuevo_nombre;
+    }
+    void setEdad(int edad) {
+        if (edad > 0)
+            this->edad = edad;
     }
     void ladrar() {
         std::cout << nombre
@@ -26,4 +54,10 @@ public:
 int main() {
     Perro p1;
     p1.ladrar();
+    Perro p2{"Fido", "Beagle", 5};
+    p2.ladrar();
+    Perro p3{"Solovino"};
+    p3.ladrar();
+    Perro p4{"Sololleva", "Golden"};
+    p4.ladrar();
 }
