c061e434f21ad025b53e7fda3304b26706c32b16
aram-gonzalez-tec <aram.gonzalez@tec.mx>
2023-11-27 08:48:37 -0600

    Solución completa

--- Ejercicio Unifila/Unifila.cpp
+++ Ejercicio Unifila/Unifila.cpp
@@ -3,9 +3,42 @@
 //
 
 #include "Unifila.h"
+#include <iostream>
+#include <vector>
 
-void Unifila::formarCliente(std::string nombre, std::string asunto) {}
+void Unifila::formarCliente(std::string nombre, std::string asunto) {
+    std::cout << "Formando al cliente: " << nombre << std::endl;
+    // clientes.push_back(Cliente{nombre, asunto});
+    clientes.emplace_back(nombre, asunto);
+}
 
-void Unifila::pasarSiguienteCliente() {}
+void Unifila::pasarSiguienteCliente() {
+    // if (clientes.size() == 0)
+    if (clientes.empty()) {
+        std::cout << "No hay clientes en la fila." << std::endl;
+    } else {
+        std::cout << "Pasando al cliente " << clientes[0].getNombre()
+                  << " con asunto " << clientes[0].getAsunto() << std::endl;
+        clientes.erase(clientes.begin());
+        /*
+        for (std::vector<Cliente>::iterator it = clientes.begin(); it != clientes.end(); it++) {
+            if (it->getNombre() == "Juan") {
+                clientes.erase(it);
+            }
+        }
+        for (Cliente &cliente : clientes)
+         */
+    }
+}
 
-void Unifila::revisarFila() {}
+void Unifila::revisarFila() {
+    if (clientes.empty()) {
+        std::cout << "No hay clientes en la fila." << std::endl;
+    } else {
+        std::cout << "Clientes formados:" << std::endl;
+        for (Cliente cliente : clientes) {
+            std::cout << "Cliente: " << cliente.getNombre()
+                      << " - Asunto: " << cliente.getAsunto() << std::endl;
+        }
+    }
+}
--- Ejercicio Unifila/main.cpp
+++ Ejercicio Unifila/main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+#include <limits>
+#include "Unifila.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Unifila unifila{};
+    int choice = 4;
+    std::string nombre, asunto;
+    while (choice != 0) {
+        std::cout << "Opciones: " << std::endl
+                  << "1) Formar un cliente." << std::endl
+                  << "2) Pasar cliente a caja." << std::endl
+                  << "3) Revisar la fila." << std::endl
+                  << "0) Salir." << std::endl
+                  << "Elija su opcion:";
+        std::cin >> choice;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        switch (choice) {
+            case 1:
+                std::cout << "Indique el nombre del cliente:";
+                std::getline(std::cin, nombre);
+                std::cout << "Indique el asunto del cliente:";
+                std::getline(std::cin, asunto);
+                unifila.formarCliente(nombre, asunto);
+                break;
+            case 2:
+                unifila.pasarSiguienteCliente();
+                break;
+            case 3:
+                unifila.revisarFila();
+                break;
+            case 0:
+                std::cout << "Hasta luego." << std::endl;
+                break;
+            default:
+                std::cout << "Debe elegir una opcion correcta." << std::endl;
+        }
+    }
     return 0;
 }
